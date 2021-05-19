2a348f7f429414be1d4aab6289e4505e66ac763a
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2021-05-19 13:46:17 +0200

    🏷️ Improve typings and add function to manage cache

--- src/memoize-function/memoize-function.function.ts
+++ src/memoize-function/memoize-function.function.ts
@@ -1,28 +1,55 @@
 import { generateCacheKey } from "../generate-cache-key/generate-cache-key.function";
 
 export const memoizeFunction = <
-  ResultFunction extends (...args: any[]) => ReturnType<ResultFunction>
+  ResultFunction extends (
+    this: any,
+    ...args: any[]
+  ) => ReturnType<ResultFunction>
 >(
   callback: ResultFunction
 ) => {
+  interface CacheHelpers {
+    get: () => Cache;
+    clear: () => void;
+    find: (key: string) => ReturnType<ResultFunction>;
+    delete: (key: string) => ReturnType<ResultFunction>;
+  }
+
+  type ResultType = ResultFunction & CacheHelpers;
   type Cache = { [key: string]: ReturnType<ResultFunction> };
 
-  const cache: Cache = {};
+  let cache: Cache = {};
 
   const memoize = (...args: any[]): ReturnType<ResultFunction> => {
     const cacheKey = generateCacheKey(...args);
     const cachedValue = cache[cacheKey];
 
     if (!cachedValue) {
-      const result = callback(...args);
+      const result = callback.apply(this, args);
       cache[cacheKey] = result;
 
       return result;
     }
 
-    console.log(cache);
     return cachedValue;
   };
 
-  return memoize as ResultFunction;
+  const parsedMemoize = memoize as ResultType;
+
+  parsedMemoize.get = () => cache;
+
+  parsedMemoize.find = (key: string) => cache[key];
+
+  parsedMemoize.delete = (key: string) => {
+    const cacheValue = cache[key];
+    delete cache[key];
+
+    return cacheValue;
+  };
+
+  parsedMemoize.clear = () => {
+    cache = {};
+  };
+
+  return memoize as ResultType;
 };
--- src/memoize-function/memoize-function.test.ts
+++ src/memoize-function/memoize-function.test.ts
@@ -1,8 +1,54 @@
 import { memoizeFunction } from "./memoize-function.function";
 
+type Input = {
+  lastname: string;
+  firstname: string;
+};
+
 type MultipleBy10Function = (value: number) => number;
+type GetFullNamesFunction = (input: Input[]) => string[];
 
 describe("memoizeFunction", () => {
+  describe("functions to manage cache", () => {
+    it("should contain get function", () => {
+      const memoized = memoizeFunction((n: number) => n * 10);
+
+      memoized(10);
+      memoized(40);
+      expect(memoized.get()).toEqual({ _10_: 100, _40_: 400 });
+    });
+
+    it("should contain find function", () => {
+      const memoized = memoizeFunction((n: number) => n * 10);
+
+      memoized(10);
+      memoized(40);
+      expect(memoized.find("")).toBeUndefined();
+      expect(memoized.find("_40_")).toEqual(400);
+    });
+
+    it("should contain delete function", () => {
+      const memoized = memoizeFunction((n: number) => n * 10);
+
+      memoized(10);
+      memoized(40);
+      expect(memoized.delete("")).toBeUndefined();
+      expect(Object.keys(memoized.get())).toHaveLength(2);
+
+      expect(memoized.delete("_10_")).toEqual(100);
+      expect(Object.keys(memoized.get())).toHaveLength(1);
+    });
+
+    it("should contain clear function", () => {
+      const memoized = memoizeFunction((n: number) => n * 10);
+
+      memoized(10);
+      memoized(40);
+      expect(memoized.clear());
+      expect(Object.keys(memoized.get())).toHaveLength(0);
+    });
+  });
+
   describe("base usage", () => {
     let multipleBy10: MultipleBy10Function;
     let memoizedMultipleBy10: MultipleBy10Function;
@@ -41,6 +87,51 @@ describe("memoizeFunction", () => {
       memoizedMultipleBy10(1);
 
       expect(multipleBy10).toHaveBeenCalledTimes(4);
+    });
+  });
+
+  describe("normal usage", () => {
+    let getFullNames: GetFullNamesFunction;
+    let memoizedGetFullNames: GetFullNamesFunction;
+
+    const inputData: Input[] = [
+      {
+        lastname: "456",
+        firstname: "123",
+      },
+      {
+        lastname: "def",
+        firstname: "abc",
+      },
+    ];
+
+    beforeEach(() => {
+      getFullNames = jest
+        .fn()
+        .mockImplementation((inputs: Input[]): string[] =>
+          inputs.map((_input) => `${_input.firstname} ${_input.lastname}`)
+        );
+
+      memoizedGetFullNames = memoizeFunction(getFullNames);
+    });
+
+    it("should return a result of a function", () => {
+      expect(memoizedGetFullNames(inputData)).toEqual(["123 456", "abc def"]);
+    });
+
+    it("should return the same result if the arguments have not changed", () => {
+      expect(memoizedGetFullNames(inputData)).toEqual(["123 456", "abc def"]);
+      expect(memoizedGetFullNames(inputData)).toEqual(["123 456", "abc def"]);
+    });
+
+    it("should not execute the memoized function if the arguments have not changed", () => {
+      memoizedGetFullNames(inputData);
+      memoizedGetFullNames(inputData);
+      memoizedGetFullNames(inputData);
+      memoizedGetFullNames(inputData);
+      memoizedGetFullNames(inputData);
+
+      expect(getFullNames).toHaveBeenCalledTimes(1);
     });
   });
 });
